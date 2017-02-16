161f96279a61b7dc23972965998e2678773726a7
Jeremy Patterson <patterson.jeremy94@outlook.com>
2017-02-15 21:52:02 -0500

    Add files

---
+++ JS/myscript.js
@@ -0,0 +1,55 @@
+<!-- 
+function validateForm() {
+	var first = document.forms["myForm"]["firstname"].value
+	if(first == "")
+	{
+		alert("First name must be filled out.");
+		return false;
+	}
+	
+	var last = document.forms["myForm"]["lastname"].value
+	if(last == "")
+	{
+		alert("Last name must be filled out.");
+		return false;
+	}
+
+	var quarterback = document.forms["myForm"]["quarterback"].value
+	if(quarterback == "")
+	{
+		alert("Your favorite quarterback must be filled out.");
+		return false;
+	}
+
+	var kicker = document.forms["myForm"]["kicker"].value
+	if(kicker == "")
+	{
+		alert("Your favorite kicker must be filled out.");
+		return false;
+	}
+
+	var favoriteTeam = document.forms["myForm"]["favoriteTeam"].value
+	if(favoriteTeam == "")
+	{
+		alert("Your favorite team must be filled out.");
+		return false;
+	}
+
+	var runningBack = document.forms["myForm"]["runningBack"].value
+	if(runningBack == "")
+	{
+		alert("Your favorite running back must be filled out.");
+		return false;
+	}
+
+	var coach = document.forms["myForm"]["coach"].value
+	if(coach == "")
+	{
+		alert("Your favorite coach must be filled out.");
+		return false;
+	}
+
+	document.getElementById("submit").innerHTML = "Your form has been submitted!";
+	return true;
+}
+-->
